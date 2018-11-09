c43706adb6a4af82987ecea1266454ec68094e9d
Krissy Dukes <krissydotnet@yahoo.com>
2018-11-09 10:33:59 -0500

    Update ajax/doggos.js

--- ajax/doggos.js
+++ ajax/doggos.js
@@ -20,15 +20,33 @@
 const BREEDS_URL = 'https://dog.ceo/api/breeds/list/all';
 
 
+const select = document.querySelector('.breeds');
+
 fetch(BREEDS_URL).then(function(response) {
     return response.json();
 }).then(function (data) {
     const breedsObject = data.message;
-    const breedsArray = Object.keys(data.message);
+    const breedsArray = Object.keys(breedsObject);
+
     for(let i = 0; i < breedsArray.length; i++) {
         const option = document.createElement('option');
         option.value = breedsArray[i];
         option.innerText = breedsArray[i];
-        document.querySelector('.breeds').appendChild(option);
+        select.appendChild(option);
     }
 })
+
+select.addEventListener("change", function(event) {
+
+    // make url
+    let url = `https://dog.ceo/api/breed/${event.target.value}/images/random`;
+
+    // show loading spinner
+
+    // fetch from the API
+    getDog(url);
+    // use the URL to change the current image
+
+    // stop showing loading spinner
+
+});
